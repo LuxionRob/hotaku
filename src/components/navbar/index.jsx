781d25a588ee6e61e60b0803c0136510c6df0376
Luxion <aidaynhi8@gmail.com>
2022-05-02 12:44:24 +0700

    update: navbar  genre

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,58 +1,65 @@
+import { useState } from 'react'
+// import { Dropdown, Button } from 'antd'
 import './style.scss'
 import logo from '../../assets/images/logo.png'
 
+const genre = [ 'Action', 'Adult', 'Adventure', 'Anime', 'Chuyển Sinh', 'Comedy', 'Comic', 'Demons', 'Detective', 'Doujinshi', 'Drama', 'Ecchi', 'Fantasy', 'Gender Bender', 'Harem', 'Historical', 'Horror', 'Huyền Huyễn', 'Isekai', 'Josei', 'Mafia', 'Magic', 'Manhua', 'Manhwa', 'Martial Arts', 'Mature', 'Military', 'Mystery', 'One shot', 'Psychological', 'Romance', 'School Life', 'Sci-fi', 'Seinen', 'Shoujo', 'Shoujo Ai', 'Shounen', 'Shounen Ai', 'Slice of life', 'Smut', 'Sports', 'Supernatural', 'Tragedy', 'Truyện Màu', 'Yaoi', 'Yuri' ]
+
 export default function Navbar() {
-  // const [ avt, setAvt ] = useState('')
+  const [ isGenreDropdownVisible, setIsGenreDropdownVisible ] = useState(true)
 
   // useEffect({
   //   // Fetch avt
   //   // Fetch user name
   //   // Fetch nofication
   // }, [])
+  
+  const handleMouseEnter = () => {
+    setIsGenreDropdownVisible(true)
+  }
+  
+  const handleMouseLeave = () => {
+    setIsGenreDropdownVisible(false)
+  }
 
   return (
-    <div>
+    <div className="navbar-container">
       <div className="navbar">
         <a href="/" className="nav-item">
           <img src={logo} className="logo" alt="logo" />
           Hotaku
         </a>
-        <div type="button" className="genre-subnavbar nav-item">
+        <div 
+          type="button"
+          className="genre-subnavbar nav-item"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onFocus={() => {}}
+        >
           Genre
-          <div className="genre-subnavbar-dropdown">
-            <a href="/search?" className="genre-subnavbar-item">
-              Shounen
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Shoujo
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Isekai
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Fantasy
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Horror
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Supernatural
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Tragedy
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Drama
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Fiction
-            </a>
-            <a href="search?" className="genre-subnavbar-item">
-              Romance
-            </a>
-          </div>
         </div>
       </div>
+      { isGenreDropdownVisible ? (
+        <div className="genre-subnav-dropdown-menu">
+          {
+            genre.map((element, index) => {
+              const link = `/search?${element}`
+              return (
+                <a
+                  key={index + 1}
+                  href={link}
+                  className="genre-subnav-dropdown-item"
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {element}
+                </a>
+              )
+            })
+          }
+        </div>
+      ) : ''
+      }
     </div>
   )
 }
